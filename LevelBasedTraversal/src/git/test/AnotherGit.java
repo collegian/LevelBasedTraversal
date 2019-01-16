83e4716647ab54f4a67203c3ae8c5a3529e41af1
Rahul Choubey <rahul.choubey@cerner.com>
2019-01-16 13:53:03 -0600

    Adding a new method + a new statement

--- LevelBasedTraversal/src/git/test/AnotherGit.java
+++ LevelBasedTraversal/src/git/test/AnotherGit.java
@@ -5,5 +5,11 @@ public class AnotherGit
     public void printGit()
     {
         System.out.println("Printing another gitter");
+        System.out.println("I am the new statement in master at 1.51 pm");
+    }
+
+    void dosome()
+    {
+        System.out.println("Adding new method in master at 1.52 pm");
     }
 }
